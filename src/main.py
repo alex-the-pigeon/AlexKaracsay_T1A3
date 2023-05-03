799cd0ce627f31eba459d4283d40eee10f8855e7
alex-the-pigeon <xelakart@gmail.com>
2023-05-03 21:00:25 +1000

    added first story

--- src/main.py
+++ src/main.py
@@ -1,1 +1,27 @@
-print("hello world")
+# import information here
+
+# ASCII art header
+
+
+# Menu
+
+
+
+
+
+# Story 1 questions/prompts
+
+s1_excited = input("Are you excited to play? Yes/No: ") # try to make this a boolean elif
+s1_fictional_character = input("Enter the name of a fictional character: ")
+s1_movement = input("Enter a type of movement eg. dance, run etc. : ")
+s1_feeling = input("Enter a type of feeling: ")
+s1_sound = input("Name a sound!: ")
+s1_adjective = input("Enter a silly adjective: ")
+
+# Story 1 - The Comedian
+
+print("THE COMEDIAN")
+madlib1 = f"The sound of the crowd starts to swell. \ Everyone is very {s1_excited} about seeing the latest stand-up show by {s1_fictional_character}. \ You {s1_movement} over to your seat in the front row and sit down.\ The show is about to start and youre so feeling so {s1_feeling}.\ {s1_fictional_character} comes out on stage and begins to {s1_sound}.\ The crowd applauds loudly. \What an {s1_adjective} show!"
+
+print(madlib1)
+
