e697506e98116f4a35cdfbdd706053d537021f61
alex-the-pigeon <xelakart@gmail.com>
2023-05-04 17:01:45 +1000

    ammended stories

--- src/main.py
+++ src/main.py
@@ -2,27 +2,40 @@
 
 # ASCII art header
 
+# Beginning of menu function 
 
-# Menu
+print("Welcome to Silly Libs!")
+
+def create_menu():
+    print("| Enter 1 to choose:  THE COMEDIAN |")
+    print("| Enter 2 to choose:  THE ATHLETE  |")
+    print("| Enter 3 to choose:  THE STUDENT  |")
+    print("| Enter 4 to to return to menu     |")
+    choice = input("Enter your selection: ")
+    return choice
+
+user_choice = ""
+
+
+while user_choice != "4":
+    user_choice = create_menu()
+
+    if (user_choice == "1"):
+        print("Start playing THE COMEDIAN?")
+    elif (user_choice == "2"):
+        print("Start playing THE ATHLETE?")
+    elif (user_choice == "3"):
+        print("Start playing THE STUDENT?")
+    elif (user_choice == "4"):
+        continue
+    else:
+        print("Invalid Input")
+
+    input("Press ENTER to continue")
+
+print("Thank you for playing SILLY LIBS!")   
 
 
 
-
-
-# Story 1 questions/prompts
-
-s1_excited = input("Are you excited to play? Yes/No: ") # try to make this a boolean elif
-s1_fictional_character = input("Enter the name of a fictional character: ")
-s1_movement = input("Enter a type of movement eg. dance, run etc. : ")
-s1_feeling = input("Enter a type of feeling: ")
-s1_sound = input("Name a sound!: ")
-s1_adjective = input("Enter a silly adjective: ")
-
-# Story 1 - The Comedian
-
-print("THE COMEDIAN")
-madlib1 = f"The sound of the crowd starts to swell. \n Everyone is very {s1_excited} about seeing the latest stand-up show by {s1_fictional_character}. \n You {s1_movement} over to your seat in the front row and sit down.\n The show is about to start and youre so feeling so {s1_feeling}.\n {s1_fictional_character} comes out on stage and begins to {s1_sound}.\n The crowd applauds loudly. \nWhat an {s1_adjective} show!"
-
-print(madlib1)
 
 
--- src/story2.py
+++ src/story2.py
@@ -3,13 +3,14 @@
 s2_activity = input("What is one of your favourite activities?: ")
 s2_number = input("What is your age?: ")
 s2_clothing = input("Tell me the colour and item of clothing you are wearing: ")
-s2_movement = input("Enter a type of movement eg. dance, run etc. :")
-s2_inredible = input("Are you having fun? Yes/No: ") # elif statement - prints incredible if yes, lacklustre if no
+s2_sound = input("Name a type of sound: ")
+s2_movement = input("Enter a type of movement eg. dance, run etc. : ")
+s2_adjective = input("Enter an adjective:  ") 
 s2_colour = input("What is your favorite colour?: ")
 
 # Story 2 - The Athlete
 
 print("THE ATHLETE")
-madlib2 = f"You have been training for what seems like eternity but you are finally here. \nIts the Olympic Games and youre ready to represent your country in {s2_activity}. \nIts a packed-out stadium of {s2_number} people and you are at the starting line in your {s2_clothing}. \nThe other athletes line up beside you and the gun goes off. \nYou start to {s2_movement} and youre in the lead. \nThe other athletes cannot keep up with your {s2_inredible} skills. \nYou win the {s2_colour} medal in the prestigious sport of {s2_activity}!"
+madlib2 = f"You have been training for what seems like eternity but you are finally here.\nIt's the Olympic Games and you're ready to represent your country in {s2_activity}.\nYou're standing in a packed-out stadium of {s2_number} people and you are at the starting line in your {s2_clothing}.\nThe other athletes line up beside you and the gun goes off which makes a loud {s2_sound}.\nYou start to {s2_movement} and you're suddenly in the lead.\nThe other athletes cannot keep up with your {s2_adjective} skills.\nYou fly across the finish line and win the {s2_colour} medal in the prestigious sport of {s2_activity}!"
 
 print(madlib2)
